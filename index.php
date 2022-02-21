2ec321e7a7f1e3bb44474cf49ea5ec441f7f94a6
jajika <90613806+jajika@users.noreply.github.com>
2022-02-21 10:10:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,32 +3,7 @@
 <title>Scaffold </title> 
 </head>
 
-<Body>
-<body background="untitled design.jpg">
-<body text="white"><p align="center">
-<br><b>Student Homepage</b></br>
-<br> St. Joseph School - La Salle </br>
-<br><font size="3"> Grade 9 </font></br>
-
-<br> <p align="left"><b><i>Links!!</i></b> </br>
-
-<br><i> Christian Living Education:</i></br>
-<br><a href= "CLE.html"> Click here!!</a>
-
-<p align="center"><i> Communication Arts in English:</font></i>
-<br><a href= "CAE.php">Click here!!</a>
-
-<p align="right"><i> Mathematics:</font></i>
-<br><a href= "Math.html">Click here!!</a>
-
-<br><p align="center"><i>Members:</i></br>
-<br>1. Ryhana Barredo</br>
-<br>2. Joinella Espina</br>
-<br>3. Matthew Tupas</br>
-<br>4. Joseph Decena</br>
-<br>5. Kentcesar Boko</br>
-<body bgcolor="pink"></font>
-
-</body>
-
-</html>
+<frameset cols="35%,30%,35%">
+<frame src="number1.html"/>
+<frame src="number3.html"/>
+<frame src="number2.html"/>
